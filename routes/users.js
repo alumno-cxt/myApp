f478dac9f14f64f472a5d3124a86055f0afdbe22
alumno-cxt <budiyo3@hotmail.com>
2015-08-01 03:04:49 +0200

    working update user on alumn webpage

--- routes/users.js
+++ routes/users.js
@@ -19,14 +19,34 @@ router.post('/', function(req, res, next){
 });
 
 /* UPDATE user */
-router.put('/', function(req, res, next){
-    usersMgr.updateUser(req.body, function(err){
-        if(!err){
-            res.status('303').end();
-        }else{
-            next(err);
+router.put('/:nick', function(req, res, next){
+    if(req.session.nick == undefined){
+        res.status('401').end();
+    }else {
+        var data = {};
+        var v = false;
+        if (req.body.email != req.session.email) {
+            data.email = req.body.email;
+            v = true;
         }
-    });
+        if (req.body.pass != '') {
+            data.pass = req.body.pass;
+            v = true;
+        }
+        if (v === false) {
+            res.status('200').end()
+        } else {
+            console.log(data);
+            usersMgr.updateUser(req.session.nick, data, function (err) {
+                if (!err) {
+                    if (req.body.email != '' && req.body.email != undefined) req.session.email = req.body.email;
+                    res.status('200').end();
+                } else {
+                    next(err);
+                }
+            });
+        }
+    }
 });
 
 /* GET users */
@@ -48,36 +68,44 @@ router.get('/', function(req, res, next){
 
 /* Check user exists */
 router.get('/:nick', function(req, res, next){
-    if(req.session.role === 'planner') {
-        if(req.query.role === 'teacher') {
-            usersMgr.isTeacher(req.params.nick, '', function (err) {
-                if (!err) {
-                    res.status('404').end();
-                } else {
-                    if (err.status == 404) {
-                        next(err);
+    if(req.params.nick === 'self'){
+        res.status('200').send({
+            nick: req.session.nick,
+            email: req.session.email,
+            role: req.session.role
+        }).end();
+    }else {
+        if (req.session.role === 'planner') {
+            if (req.query.role === 'teacher') {
+                usersMgr.isTeacher(req.params.nick, '', function (err) {
+                    if (!err) {
+                        res.status('404').end();
                     } else {
-                        res.status('200').end();
+                        if (err.status == 404) {
+                            next(err);
+                        } else {
+                            res.status('200').end();
+                        }
                     }
-                }
-            });
-        }
-        if(req.query.role === 'alumn') {
-            usersMgr.isAlumn(req.params.nick, '', function (err) {
-                if (!err) {
-                    res.status('404').end();
-                } else {
-                    if (err.status == 404) {
-                        next(err);
+                });
+            }
+            if (req.query.role === 'alumn') {
+                usersMgr.isAlumn(req.params.nick, '', function (err) {
+                    if (!err) {
+                        res.status('404').end();
                     } else {
-                        res.status('200').end();
+                        if (err.status == 404) {
+                            next(err);
+                        } else {
+                            res.status('200').end();
+                        }
                     }
-                }
-            });
+                });
+            }
+        } else {
+            //Unauthorized
+            res.status('401').end();
         }
-    }else{
-        //Unauthorized
-        res.status('401').end();
     }
 });
 
